712d41c84cba3b1a12f3f1db4b6c0da835a3921c
Rob Hoelz <rob@hoelz.ro>
2016-08-18 23:42:37 -0500

    Removed unused functions from forest module

--- IdeaFight/PartialForest.elm
+++ IdeaFight/PartialForest.elm
@@ -1,8 +1,5 @@
 -- a partially ordered forest
-module IdeaFight.PartialForest exposing (Forest, choose, drawForest, empty, fromList, getNextPair, isEmpty, topN, topNCount)
-
--- XXX DEBUG
-import Html exposing (Html, li, ul, text)
+module IdeaFight.PartialForest exposing (Forest, choose, fromList, getNextPair, topN)
 
 type Node a = Node a (List (Node a))
 type Forest a = Forest (List (Node a))
@@ -12,15 +9,6 @@ makeNode value = Node value []
 
 fromList : List a -> Forest a
 fromList values = Forest <| List.map makeNode values
-
-empty : Forest a
-empty = fromList []
-
-isEmpty : Forest a -> Bool
-isEmpty (Forest values) =
-  case values of
-    [] -> True
-    _  -> False
 
 getNextPairNodes : List (Node a) -> Maybe (a, a)
 getNextPairNodes nodes =
@@ -38,14 +26,6 @@ reparentNode (Node value children) child = Node value (child :: children)
 nodeValue : Node a -> a
 nodeValue (Node value _) = value
 
-drawNode : Node a -> Html msg
-drawNode (Node value children) =
-  li [] [ (text <| toString value), ul [] (List.map drawNode children) ]
-
-drawForest : Forest a -> Html msg
-drawForest (Forest nodes) =
-  ul [] <| List.map drawNode nodes
-
 chooseNodes : List (Node a) -> a -> List (Node a)
 chooseNodes nodes choice =
   case nodes of
@@ -60,14 +40,6 @@ chooseNodes nodes choice =
 choose : Forest a -> a -> Forest a
 choose (Forest values) choice =
   Forest <| chooseNodes values choice
-
-topNCount : Forest a -> Int
-topNCount (Forest nodes) =
-  let topNCountNodes = \nodes ->
-      case nodes of
-        [(Node _ children)] -> 1 + (topNCountNodes children)
-        _ -> 0
-  in topNCountNodes nodes
 
 topNNodes : List (Node a) -> List a
 topNNodes nodes =
