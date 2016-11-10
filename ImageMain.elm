87b231b9c10661bd5472d3d57811aac5e589db49
Rob Hoelz <rob@hoelz.ro>
2016-11-09 18:38:17 -0600

    Add a rudimentary application for ordering images

---
+++ ImageMain.elm
@@ -0,0 +1,58 @@
+import Html.App as App
+import Html exposing (Html, img)
+import Html.Attributes as Attributes
+
+import IdeaFight.Compete as Compete
+
+type alias Image = String
+
+makeImage : Image -> Html msg
+makeImage src =
+  img [ Attributes.src src ] []
+
+imageIdeaFunctions : Compete.IdeaFunctions Image
+imageIdeaFunctions = {
+    renderChoice = makeImage,
+    renderResult = makeImage
+  }
+
+type Model = CompeteModel (Compete.Model Image)
+type Msg = CompeteMsg (Compete.Msg Image)
+
+images : List Image
+images = [
+    "http://localhost:5000/1.jpg",
+    "http://localhost:5000/2.jpg",
+    "http://localhost:5000/3.jpg",
+    "http://localhost:5000/4.jpg",
+    "http://localhost:5000/5.jpg",
+    "http://localhost:5000/6.jpg",
+    "http://localhost:5000/7.jpg",
+    "http://localhost:5000/8.jpg"
+  ]
+
+init : (Model, Cmd Msg)
+init =
+    let (innerModel, cmd) = Compete.init imageIdeaFunctions images
+    in (CompeteModel innerModel, Cmd.map CompeteMsg cmd)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update (CompeteMsg innerMsg) (CompeteModel innerModel) =
+  let (innerModel, cmd) =  Compete.update innerMsg innerModel
+  in (CompeteModel innerModel, Cmd.map CompeteMsg cmd)
+
+subscriptions : Model -> Sub Msg
+subscriptions (CompeteModel innerModel) =
+  Sub.map CompeteMsg <| Compete.subscriptions innerModel
+
+view : Model -> Html Msg
+view (CompeteModel innerModel) =
+  App.map CompeteMsg <| Compete.view innerModel
+
+main : Program Never
+main = App.program {
+    init = init,
+    update = update,
+    subscriptions = subscriptions,
+    view = view
+  }
