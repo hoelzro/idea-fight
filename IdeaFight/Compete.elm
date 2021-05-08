9fbb6e45a2902fed56dc63a331aa2549dd42752f
Rob Hoelz <rob@hoelz.ro>
2021-05-08 12:52:03 -0500

    elm-format compete module

--- IdeaFight/Compete.elm
+++ IdeaFight/Compete.elm
@@ -1,4 +1,4 @@
-module IdeaFight.Compete exposing (Model, Msg, decodeModel, encodeModel, init, subscriptions, update, view, topValues)
+module IdeaFight.Compete exposing (Model, Msg, decodeModel, encodeModel, init, subscriptions, topValues, update, view)
 
 import Browser.Events as Events
 import Char
@@ -12,6 +12,7 @@ import Json.Encode as Encode
 import Random
 import String
 
+
 type Model idea
     = Uninitialized
     | Initialized (Forest.Forest idea)
@@ -22,7 +23,10 @@ type Msg idea
     | Choice idea
     | NoOp
 
-type alias Renderer idea = idea -> Html (Msg idea)
+
+type alias Renderer idea =
+    idea -> Html (Msg idea)
+
 
 init : List idea -> ( Model idea, Cmd (Msg idea) )
 init lines =
@@ -51,7 +55,8 @@ update msg model =
 
 decodeKeyPress : idea -> idea -> Decode.Decoder (Msg idea)
 decodeKeyPress left right =
-  Decode.map (keyToMsg left right) <| Decode.field "key" Decode.string
+    Decode.map (keyToMsg left right) <| Decode.field "key" Decode.string
+
 
 keyToMsg : idea -> idea -> String -> Msg idea
 keyToMsg left right code =
@@ -122,20 +127,31 @@ view renderChoice renderTopValue model =
                 , topValuesSoFar renderTopValue forest
                 ]
 
+
 decodeModel : Decode.Decoder (Model String)
-decodeModel = Decode.field "nodes" <| Decode.map Initialized Forest.decodeJSON
+decodeModel =
+    Decode.field "nodes" <| Decode.map Initialized Forest.decodeJSON
 
 
-encodeModel : Model String -> List (String, Encode.Value)
+encodeModel : Model String -> List ( String, Encode.Value )
 encodeModel model =
-  case model of
-    Uninitialized -> []
-    Initialized forest ->
-      let encodedForest = Forest.encodeJSON forest
-      in [("nodes", encodedForest)]
+    case model of
+        Uninitialized ->
+            []
+
+        Initialized forest ->
+            let
+                encodedForest =
+                    Forest.encodeJSON forest
+            in
+            [ ( "nodes", encodedForest ) ]
+
 
 topValues : Model idea -> List idea
-topValues model = 
+topValues model =
     case model of
-        Uninitialized -> []
-        Initialized forest -> Forest.topN forest
+        Uninitialized ->
+            []
+
+        Initialized forest ->
+            Forest.topN forest
